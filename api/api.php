25223c7dd96cca404f662cb9af08e36f47e2c187
Ruben Mbiha <80261880+MbihaR@users.noreply.github.com>
2023-04-16 11:21:12 +0300

    Update api.php

--- api/api.php
+++ api/api.php
@@ -4,14 +4,14 @@
 function get_card_balance($card_number) {
 //   // Connect to the database
 //   $conn = mysqli_connect('localhost', 'username', 'password', 'database');
-// Connect to the database
-// define('DB_SERVER', 'containers-us-west-2.railway.app');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '1dN7r3mU2jkyJznmRxWJ');
-// define('DB_NAME', 'railway');
+Connect to the database
+define('DB_SERVER', 'containers-us-west-2.railway.app:8042');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1dN7r3mU2jkyJznmRxWJ');
+define('DB_NAME', 'railway');
 
 /* Attempt to connect to MySQL database */
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
   // Prepare a SQL statement to retrieve the balance
   $stmt = mysqli_prepare($conn, 'SELECT card_amount FROM customers WHERE card_number = ?');
@@ -33,7 +33,7 @@ $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 // Define a function to post data to the sales table
 function post_card_sales($card_number, $amount, $price, $customer_id) {
   // Connect to the database
-define('DB_SERVER', 'containers-us-west-2.railway.app');
+define('DB_SERVER', 'containers-us-west-2.railway.app:8042');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '1dN7r3mU2jkyJznmRxWJ');
 define('DB_NAME', 'railway');
@@ -56,7 +56,7 @@ $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Define a function to post data to the sales table
 function post_coin_sales($card_number, $amount, $price, $customer_id) {
     // Connect to the database
-  define('DB_SERVER', 'containers-us-west-2.railway.app');
+  define('DB_SERVER', 'containers-us-west-2.railway.app:8042');
   define('DB_USERNAME', 'root');
   define('DB_PASSWORD', '1dN7r3mU2jkyJznmRxWJ');
   define('DB_NAME', 'railway');
