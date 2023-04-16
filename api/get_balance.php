d5c1ad44034368517a35301bb8bec3c6b8935d1c
Ruben Mbiha <80261880+MbihaR@users.noreply.github.com>
2023-04-16 10:26:03 +0300

    Update get_balance.php

--- api/get_balance.php
+++ api/get_balance.php
@@ -7,11 +7,9 @@ require_once('api.php');
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['card_number'])) {
   $card_number = $_GET['card_number'];
   $balance = get_card_balance($card_number);
-  $response = array('balance' => $balance);
-  header('Content-Type: application/json');
-  echo json_encode($response);
+  //header('Content-Type: application/json');
+  echo $balance;
 } else {
   header('HTTP/1.1 400 Bad Request');
   echo "Invalid request.";
 }
-?>
