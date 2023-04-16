a58884f1182f3e50d8ca198691b0a2a347275a1d
Ruben Mbiha <80261880+MbihaR@users.noreply.github.com>
2023-04-16 10:27:38 +0300

    Update post_card_sales.php

--- api/post_card_sales.php
+++ api/post_card_sales.php
@@ -1,24 +1,27 @@
 <?php
 
 // Include the API file that contains the post_sales_data() function
-require_once('card_api.php');
+require_once('api.php');
 
 // Define the endpoint
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['card_number']) && isset($_GET['amount']) && isset($_GET['item']) && isset($_GET['machine_id'])) {
-  $card_number = $_GET['card_number'];
-  $amount = floatval($_GET['amount']);
-  $machine_id = intval($_GET['machine_id']);
+  $customer_id = $_GET['card_number'];
+  $amount = $_GET['amount'];
+  $machine_id = $_GET['machine_id'];
   $item = $_GET['item'];
 
-  if($card_number != null){
-    post_card_sales($card_number, $amount, $machine_id, $item);
-    echo "Sale successful!";
-  }
-  else{
-    post_coin_sales($card_number, $amount, $machine_id, $item);
-    echo "Sale successful!";
-  }
-  
+  echo $customer_id, $amount, $machine_id,  $item;
+
+    if($customer_id != null){
+        post_card_sales($machine_id, $item,  $amount, $customer_id);
+        echo "Sale card successful!";
+    }
+    else{
+        post_coin_sales($machine_id, $item,  $amount);
+        echo "Sale coin successful!";
+    }
+    
+ 
 
 } else {
   header('HTTP/1.1 400 Bad Request');
