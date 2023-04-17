d6197b38fbeb6c943d8a514cd89e80856d5f5b44
Ruben Mbiha <80261880+MbihaR@users.noreply.github.com>
2023-04-17 11:29:03 +0300

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,14 +1,15 @@
 <?php
-
-// Define a function to get the card balance
-function get_card_balance($card_number) {
-//   // Connect to the database
-//   $conn = mysqli_connect('localhost', 'username', 'password', 'database');
 // Connect to the database
 define('DB_SERVER', 'containers-us-west-2.railway.app:8042');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '1dN7r3mU2jkyJznmRxWJ');
 define('DB_NAME', 'railway');
+
+// Define a function to get the card balance
+function get_card_balance($card_number) {
+//   // Connect to the database
+//   $conn = mysqli_connect('localhost', 'username', 'password', 'database');
+
 
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
