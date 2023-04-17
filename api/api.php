e47b78feea3859257b2176bead80399e02ad8945
Ruben Mbiha <80261880+MbihaR@users.noreply.github.com>
2023-04-17 11:26:21 +0300

    Update api.php

--- api/api.php
+++ api/api.php
@@ -95,12 +95,6 @@ define('DB_NAME', 'railway');
 }
 
 function reduce_card_balance($card_id) {
-  // Connect to the database
-  define('DB_SERVER', 'containers-us-west-2.railway.app:8042');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', '1dN7r3mU2jkyJznmRxWJ');
-  define('DB_NAME', 'railway');
-
   /* Attempt to connect to MySQL database */
   $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
